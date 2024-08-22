9c6f40d2ad8e594bcca0b846bb66f1bf5671839b
Ashish05122004 <ashishpradhan130@gmail.com>
2024-08-22 10:49:55 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('.hamburger');
+const menuBar = document.querySelector('.menu-bar');
+const menuList = document.querySelector('.nav-menu');
+
+menuBtn.addEventListener('click', showMenu);
+
+function showMenu(){
+    menuBtn.classList.toggle('is-active');
+    menuBar.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+}
+
+var typed = new Typed('#element', {
+    strings: ['Ashish Pradhan','Java Developer','Web Designer','Web Developer'],
+    typeSpeed: 150,
+    loop:true
+  });
